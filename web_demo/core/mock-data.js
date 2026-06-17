53ee014e21dcecf6cf2c00c08ae5803f1710748d
yu5-520 <zhengyeyu520@gmail.com>
2026-06-17 15:15:24 +0800

    feat: add v3 mock data fields

--- web_demo/core/mock-data.js
+++ web_demo/core/mock-data.js
@@ -6,5 +6,7 @@
     traffic: [],
     reportGroups: [],
     reportDetails: {},
+    v3: { version: "3.0.0", activeAlertCount: 0, highPriorityAlertCount: 0, latestAlerts: [] },
+    recentAlerts: [],
   };
 })();
