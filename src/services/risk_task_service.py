de1e197c4e3553ca560fdc23b313fde80beddac5
yu5-520 <zhengyeyu520@gmail.com>
2026-06-23 21:47:18 +0800

    chore: point risk task facade to V6.6

--- src/services/risk_task_service.py
+++ src/services/risk_task_service.py
@@ -1,8 +1,1 @@
-"""Compatibility wrapper for the current V6.5 risk task service."""
-
-from src.services.risk_task_v65_service import (  # noqa: F401
-    RISK_TASK_VERSION,
-    ensure_risk_task_tables,
-    generate_risk_tasks_for_signals,
-    risk_task_summary,
-)
+from src.services.risk_task_v66_service import RISK_TASK_VERSION, ensure_risk_task_tables, generate_risk_tasks_for_signals, risk_task_summary
