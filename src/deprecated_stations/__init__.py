a16b5d6c191287ca2fca4d89e0f2766b2b1bc3f4
yu5-520 <zhengyeyu520@gmail.com>
2026-06-29 21:05:36 +0800

    V12.14.3 add deprecated stations package

---
+++ src/deprecated_stations/__init__.py
@@ -0,0 +1,5 @@
+"""Deprecated Station Archive package.
+
+Files in this package are not part of the business mainline. They are stored for
+history, migration reference and deletion governance only.
+"""
