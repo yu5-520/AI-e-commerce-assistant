ff4bb51ed7a9a4691ada5f0f649436c3717995fd
yu5-520 <zhengyeyu520@gmail.com>
2026-06-29 21:05:44 +0800

    V12.14.3 add archive services package

---
+++ src/deprecated_stations/archive_services/__init__.py
@@ -0,0 +1,6 @@
+"""Archive-only legacy service files.
+
+Importing these modules from mainline code is forbidden. Deprecated Station
+Archive records each file's original path, old purpose, replacement station and
+delete-after version.
+"""
